2c006fcc3e037e7bb3505a0f6b5af0b672b3ffd1
JujuYuki <me@jujuyuki.fr>
2019-03-26 22:11:49 +0000

    Unlimited rulebook

--- Yotsugi/main.go
+++ Yotsugi/main.go
@@ -16,4 +16,14 @@ func main() {
 		Server:			"irc.chat.twitch.tv",
 	}
 	yotsugi.Start()
+	yotsugi2 := twitchBot.BasicBot{
+		Channel:		"jujuyuki",
+		Owner:			"jujuyuki",
+		MsgRate:		time.Duration(20/30)*time.Millisecond,
+		Name:			"Yotsugi_chan",
+		Port:			"6667",
+		Private:		"./private/oauth.json",
+		Server:			"irc.chat.twitch.tv",
+	}
+	yotsugi2.Start()
 }
